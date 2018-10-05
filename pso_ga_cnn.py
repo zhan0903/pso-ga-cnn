6447b04e6172b44bfab54ea021462e138d426d78
zhan0903 <z.han0903@qq.com>
2018-10-05 16:00:01 +1000

    9/26 version pso+ga+cnn

--- pso_ga_cnn.py
+++ pso_ga_cnn.py
@@ -100,11 +100,11 @@ def work_func(input_w):
     game = input_w[2]
     device = input_w[3]
     env_w = make_env(game)
-    parent_net_w = Net(env_w.observation_space.shape, env_w.action_space.n).to(device)
+    parent_net_w = Net(env_w.observation_space.shape, env_w.action_space.n)
     print("in work_fun,device:{}".format(device))
     print("in work_func, parent_net:{}".format(parent_net['fc.2.bias']))
     parent_net_w.load_state_dict(parent_net)
-    child_net = mutate_net(parent_net_w, seed_w, device, copy_net=False).to(device)
+    child_net = mutate_net(parent_net_w.to(device), seed_w, device, copy_net=False).to(device)
     reward, frames = evaluate(child_net, env_w).to(device)
     result = (seed_w, reward, frames)
     return result
